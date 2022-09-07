4b14e2680a91582e590537a9e26a097acbdbefe4
Shreyash <shreyashkumarshrivastava7999@gmail.com>
2022-09-07 22:58:38 +0530

    Block Pe 2.0

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express=require("express");
 const app=express();
+const hostname='0.0.0.0';
 require("./db/conn")
 app.use(express.json());
 app.use(require('./Router/auth'));
@@ -8,6 +9,6 @@ app.get("/",(req,res)=>{
     res.send("Welcome to block Pe")
 })
 
-app.listen(process.env.PORT||3000,()=>{
+app.listen(process.env.PORT||3000,hostname,()=>{
     console.log("Server running at port 3000")
 })
