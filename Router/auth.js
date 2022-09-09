076501daf71130c5395dc4c7926a3391d62ec7ad
Shreyash <shreyashkumarshrivastava7999@gmail.com>
2022-09-09 16:35:10 +0530

    gsfhdf

--- Router/auth.js
+++ Router/auth.js
@@ -46,6 +46,11 @@ router.post('/register',(req,res)=>{
 
 router.post('/Login',async (req,res)=>{
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+
     try{
         const {email,password}=req.body;
         if(!email || !password){
--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 const express=require("express");
+const cors=require("cors");
 const app=express();
 const hostname='0.0.0.0';
 require("./db/conn")
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(require('./Router/auth'));
 app.use(require('./Router/purchase'))
 app.get("/",(req,res)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+
+
     res.send("Welcome to block Pe")
 })
 
-app.listen(process.env.PORT||3000,hostname,()=>{
+app.listen(process.env.PORT||5000,hostname,()=>{
     console.log("Server running at port 3000")
 })
