de581266d7a5594de26d297e38370d8d09b7df02
Shreyash <shreyashkumarshrivastava7999@gmail.com>
2022-09-14 17:25:53 +0530

    hgf

--- Router/purchase.js
+++ Router/purchase.js
@@ -1,4 +1,8 @@
+//importing all neccessary modules
+
 const express=require('express');
+var nodemailer = require('nodemailer');
+const twilio = require('twilio');
 const cors=require("cors");
 const pur=express.Router();
 require('../db/conn')
@@ -6,7 +10,26 @@ const Purchase=require('../models/Transaction detail.js')
 pur.use(cors({
     origin: '*'
 }));
+//Setting up Whatsapp and Email communication
+// for sending bills
+
+const id = 'ACa95ba9947c6361364919e69943aef559';
+const token = '4b9d9a501c6056a0f1a0e0f3ed140083';
+const client = twilio(id, token);
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'blockpeofficialbill@gmail.com',
+      pass: 'dkcqtuicrmaiiznc'
+    }
+  });
+
+
+//Route to purchase Product
+
 pur.post('/purchase',async (req,res)=>{
+    //Validating whether or not all details have been submitted from front-end
     const {StoreName,Invoice,PCategory,ProductName,ProductPrice,DOP, CustomerName,AadharCard, email,phone,MIStoreID,Hash}=req.body;
 
     if(!StoreName || !Invoice || !ProductName || !ProductPrice || !DOP || !CustomerName || !PCategory||!AadharCard|| !email ||!phone||!MIStoreID||!Hash){
@@ -15,7 +38,7 @@ pur.post('/purchase',async (req,res)=>{
         })
     }
     else{
-    const bill=new Purchase({
+    const bill2={
         Hash:Hash,
         StoreName:StoreName,
         Invoice:Invoice,
@@ -28,11 +51,61 @@ pur.post('/purchase',async (req,res)=>{
         email:email,
         phone:phone,
         MIStoreID:MIStoreID
+    }
+    const bill=new Purchase({
+       ...bill2
     })
     bill.save().then(()=>{
         res.status(200).json({
             message:"Bill made successfully"
     })
+    var mailOptions = {
+        from: 'shreyashkumarshrivastava7999@gmail.com',
+        to: `${bill2.email}`,
+        subject: `Your Bill from Block Pe for ${bill2.ProductName}`,
+        text: `CustomerName\t${bill2.CustomerName}\n
+        Invoice\t${bill2.Invoice}\n
+        Hash\t${bill2.Hash}\n
+        StoreName\t${bill2.StoreName}\n
+        MI Store ID\t${bill2.MIStoreID}\n
+        Date of Purchase\t${bill2.DOP}\n
+        Price\t${bill2.ProductPrice}\n
+          
+        `
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+      client.messages
+ .create({
+  
+  // Bill to be sent..
+  body: `Your Bill for ${bill2.ProductName}\nCustomerName\t${bill2.CustomerName}\n
+  Invoice\t${bill2.Invoice}\n
+  Hash\t${bill2.Hash}\n
+  StoreName\t${bill2.StoreName}\n
+  MI Store ID\t${bill2.MIStoreID}\n
+  Date of Purchase\t${bill2.DOP}\n
+  Price\t${bill2.ProductPrice}\n
+    
+  `,
+
+  // Senders Number (Twilio Sandbox No.)
+  from: 'whatsapp:+14155238886',
+
+  // Number receiving the message
+  to: `whatsapp:+91${bill2.phone}`
+ })
+ .then(message => console.log("Message sent successfully"))
+ .done();
+
+
 }).catch((err)=>{
     console.log(err)
     res.status(500).json({error:"Can't complete the bill"})
